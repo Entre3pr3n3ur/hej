0e5f44b98c85e8b0fce0dde6849e21ec47850fb8
Entre3pr3n3ur <kevindavidsen91@gmail.com>
2024-08-14 21:21:32 +0200

    airline tilføjet
    Første gang airline er tilføjet til GitHub for hjælp til oprettelse af test miljø

---
+++ airline/users/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+# create urlpatterns here
+urlpatterns = [
+    path('', views.index, name="index"),
+    path("login", views.login_view, name="login"),
+    path("logout", views.logout_view, name="logout"),
+]
